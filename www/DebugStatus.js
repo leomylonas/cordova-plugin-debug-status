d57308d6d48c552a79c90f11f556dbcd66217538
Leo Mylonas <leo@mylonas.co>
2016-01-25 13:18:58 +1000

    This should be an exported module, not an object.

--- www/DebugStatus.js
+++ www/DebugStatus.js
@@ -1,4 +1,4 @@
-window.DebugStatus = {
+module.exports = {
 	getDebugStatus: function(successCallback, errorCallback) {
 		cordova.exec(successCallback, errorCallback, "DebugStatus", "getDebugStatus", []);
 	}
